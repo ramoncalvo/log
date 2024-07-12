3746a7fc914cb788efc6d331691631cbe8707231
RamonCalvo <ramon.calvo@gmail.com>
2024-07-11 22:20:31 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+import { Console as log } from "./utils.js";
+
+// Usage example
+log.showLogs = true;  // Set to false to disable logging
+
+// Test the logging behavior
+log.info("This is a normal log message from Console.log.");
+log.errorLog("This is an error message from Console.errorLog.");
+
+// Toggle the logging behavior
+// Console.showLogs = false;
+log.warn('Hello', 'World!', 'Foo', 'bar', 'biz', 'baz');
+log.errorLog("This error message will not be shown.");
---
+++ utils.js
@@ -0,0 +1,51 @@
+import chalk from 'chalk';
+
+export class Console {
+    // External variable to control logging behavior
+    static showLogs = true;
+
+    // Helper method to get the caller line number
+    static getCallerLine() {
+        const stack = (new Error()).stack.split('\n');
+        // The 3rd element in the stack array should be the caller (index 2 because arrays are 0-indexed)
+        const caller = stack[3].trim();
+        return caller;
+    }
+
+    // Method to log messages
+    static info(...messages) {
+        if (Console.showLogs) {
+            const callerLine = Console.getCallerLine();
+            const line = callerLine.split('/')
+            console.log('-')
+            console.log(`${chalk.cyan('Info:')} ${line[line.length -1]}]`,
+                  ...messages
+                );
+            
+        }
+    }
+    // Method to log messages
+    static warn(...messages) {
+        const warning = chalk.hex('#FFA500');
+        if (Console.showLogs) {
+            const callerLine = Console.getCallerLine();
+            const line = callerLine.split('/')
+            console.log('-')
+            console.log(`${warning('Warn:')} ${line[line.length -1]}]`,
+                  ...messages
+                );
+            
+        }
+    }
+    
+    // Method to log error messages in red
+    static errorLog(...messages) {
+        if (Console.showLogs) {
+            const callerLine = Console.getCallerLine();
+            const line = callerLine.split('/')
+            console.log('-')
+            console.log(chalk.red('Error'), `${chalk.green('[File & Line No:')} ${line[line.length - 1]}]`, ...messages.map(message => chalk.blue.bgRed.bold(message)));
+            
+        }
+    }
+}
